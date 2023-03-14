474cf8a7cdd8142e287faac13088979d51299d33
Алексей <electrifyx@gmail.com>
2023-03-14 13:20:31 +0200

    edit on async-await

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,14 +1,19 @@
 const BASE_URL = 'https://restcountries.com';
 
-function fetchCountries(name) {
-  return fetch(
-    `${BASE_URL}/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+async function fetchCountries(name) {
+  const response = await fetch(`${BASE_URL}/v3.1/name/
+  ${name}?fields=name,capital,population,flags,languages`);
+  return response;
 }
+// return await fetch(
+//   `${BASE_URL}/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+// ).then(response => {
+//   if (!response.ok) {
+//     throw new Error(response.status);
+//   }
+//   return response.json();
+// });
+
+// }
 
 export { fetchCountries };
