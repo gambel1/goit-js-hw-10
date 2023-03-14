76019a92630cfab77998b922b9402bac8be3b975
Алексей <electrifyx@gmail.com>
2023-03-14 21:24:32 +0200

    edit fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,8 @@
 const BASE_URL = 'https://restcountries.com';
 
 async function fetchCountries(name) {
-  const response = await fetch(`${BASE_URL}/v3.1/name/
-  ${name}?fields=name,capital,population,flags,languages`);
+  const response = await fetch(
+    `${BASE_URL}/v3.1/name/${name}?fields=name,capital,population,flags,languages`);
   return response;
 }
 // return await fetch(
