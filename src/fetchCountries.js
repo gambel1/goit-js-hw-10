84d0088dde8beccce5305f1a29a3201e4a5a59cd
Алексей <electrifyx@gmail.com>
2023-03-15 15:23:46 +0200

    finishid homework

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,7 +3,7 @@ const BASE_URL = 'https://restcountries.com';
 async function fetchCountries(name) {
   const response = await fetch(
     `${BASE_URL}/v3.1/name/${name}?fields=name,capital,population,flags,languages`);
-  return response;
+  return response.json();
 }
 // return await fetch(
 //   `${BASE_URL}/v3.1/name/${name}?fields=name,capital,population,flags,languages`
